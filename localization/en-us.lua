f473915fe0a3fdbddbc12818153fee2ad3f3b338
MullMayhem <dejiliptakfurryart@gmail.com>
2025-05-05 14:38:08 -0400

    Update and rename default.lua to en-us.lua

---
+++ localization/en-us.lua
@@ -0,0 +1,87 @@
+return {
+    descriptions={
+        alt_texture = {
+            alt_tex_swagtro_j_SwagLegends = {
+                name = 'Swaglandia Legends',
+            }
+        },
+    },
+    misc = {
+        quips={
+            dq_1={
+                "Yikes! I hope you",
+                "have a few tricks",
+                "up your sleeve for",
+                "this final challenge!",
+            },
+            lq_1={
+                "I've out woked",
+                "you today, get",
+                "fucked...",
+            },
+            lq_10={
+                "What a flop!",
+            },
+            lq_2={
+                ":troll:",
+            },
+            lq_3={
+                "Is it Wine",
+                "Friday yet?",
+            },
+            lq_4={
+                "don't worry, the",
+                "foods not hot",
+            },
+            lq_5={
+                "hey siri, google",
+                "how to get game",
+            },
+            lq_6={
+                "maybe Battlefield",
+                "is more your speed...",
+            },
+            lq_7={
+                "wait, this isn't",
+                "Draconic Evolution!",
+            },
+            lq_8={
+                "I think I am drank",
+            },
+            lq_9={
+                "Damn I could have been",
+                "eating food this whole time,", 
+                "too bad!",
+            },
+            wq_1={
+                "I'M GONNA JACK OFFFFFFFFFFFFFFF",
+            },
+            wq_2={
+                "Oo oo oo oo I like him",
+                "being on top... wait actually",
+            },
+            wq_3={
+                "You've got my respect, but",
+                "you RP in general and it'll",
+                "be all gone",
+            },
+            wq_4={
+                "Too bad these",
+                "chips are all",
+                "virtual...",
+            },
+            wq_5={
+                "Yippee :D",
+            },
+            wq_6={
+                "I don't need",
+                "therapy, I",
+                "need you",
+            },
+            wq_7={
+                "You did it!!",
+                "Can we do SlashCo now?",
+            },
+        }
+    }
+}
