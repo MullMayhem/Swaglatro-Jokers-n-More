dbc9cc7f07b78baa2284788754ca30358dc9eb91
MullMayhem <210096436+MullMayhem@users.noreply.github.com>
2025-07-01 12:33:47 -0400

    wq/lq additions + fixes

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -53,6 +53,58 @@ return {
                 "eating food this whole time,", 
                 "too bad!",
             },
+            lq_11={
+                "im gonna menu myself",
+            },
+            lq_12={
+                "I'm not putting that in",
+                "Sworglandia 3",
+            },
+            lq_13={
+                "This vexes me",
+            },
+            lq_14={
+                "You spelled Glutenous wrong",
+            },
+            lq_15={
+                "Sorry, I ated all your score cards",
+            },
+            lq_16={
+                "kill yourself",
+            },
+            lq_17={
+                "Can someone explain the joke",
+                "I don't get it",
+            },
+            lq_18={
+                "It's like you are in some",
+                "type of {C:red}Snowgrave{}",
+            },
+            lq_19={
+                "someone should turn off",
+                "your internet",
+            },
+            lq_20={
+                "I'm going to blow you up",
+                "with my mind",
+            },
+            lq_21={
+                "I'm going to spoil today's",
+                "Wordle, its {C:attention}Your Mom{}",
+            },
+            lq_22={
+                "girlfail!!!!!",
+            },
+            lq_23={
+                "I hate Coldplay now, thanks",
+            },
+            lq_24={
+                "wow.... nice job......",
+                "you killed it...",
+            },
+            lq_25={
+                "fuck this, I'm going to work",
+            },
             wq_1={
                 "I'M GONNA JACK OFFFFFFFFFFFFFFF",
             },
@@ -66,9 +118,7 @@ return {
                 "be all gone",
             },
             wq_4={
-                "Too bad these",
-                "chips are all",
-                "virtual...",
+                "I love boobs!!!!!!!",
             },
             wq_5={
                 "Yippee :D",
@@ -81,6 +131,68 @@ return {
             wq_7={
                 "You did it!!",
                 "Can we do SlashCo now?",
+            },
+            wq_8={
+                "shove cards up my asssss",
+            },
+            wq_9={
+                "Happy birthday Nitro!",
+            },
+            wq_10={
+                "Another win for Swag!",
+            },
+            wq_11={
+                "Bartholomew",
+            },
+            wq_12={
+                "I feel so gubby rn",
+            },
+            wq_13={
+                "Get pregnant Aurori",
+            },
+            wq_14={
+                "She perke on my o",
+                "till I cryptid",
+            },
+            wq_15={
+                "meow",
+            },
+            wq_16={
+                "yall are lowkey sleeping",
+                "on balatro porn",
+            },
+            wq_17={
+                "Thanks Boobley",
+            },
+            wq_18={
+                "ERASE THAT SHIT",
+                "YOU DIRTY BOY",
+            },
+            wq_19={
+                "hey gurl~",
+            },
+            wq_20={
+                "Jarvis, give this",
+                "run a TRUE",
+            },
+            wq_21={
+                "Oh oops I wasn't supposed to",
+                "let that happen. Can you",
+                "Alt + F4 real quick :3",
+            },
+            wq_22={
+                "Congrats from me, the",
+                "real John Balatro",
+            },
+            wq_23={
+                "Hold R for a free",
+                "win next game!",
+            },
+            wq_24={
+                "gok, remember this win",
+            },
+            wq_25={
+                "Have a horse-tastic game!",
             },
         }
     }
